dc1f995a2fc9b4fafe29ea3dff043bd692aade83
maggiedougherty <maggiedougherty21@gmail.com>
2025-08-24 15:31:11 -0500

    Map updates

---
+++ Code/MH race by TRR_2020-2024_Community Area.R
@@ -0,0 +1,65 @@
+# MEDILL INVESTIGATIVE LAB
+# Author: Maggie Dougherty
+# Date: January 20, 2025
+# Updated: Aug 24, 2025 to convert to Community Area
+# Topic: Chicago PD UoF 2020-2024
+
+# set working directory
+setwd("/Users/MaggieDougherty/Documents/GitHub/CPD-Mental-Health/")
+
+# install packages
+install.packages('readxl')
+install.packages('tidyverse')
+install.packages('tidyr')
+install.packages('xlsx')
+install.packages('writexl')
+install.packages("dplyr")
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+# empty old data (if applicable)
+ls()
+rm(raw_trr, mh)
+
+# import data
+raw_trr <- read_excel("Raw Data/Copy of Chicago UoF 2020-2024.xlsx", sheet = "TRR DATA")
+
+# limit to cases flagged as mental illness related in either the Subj Condition or Type Activity columns
+mh <- raw_trr[
+  grepl("MENTAL ILL./EMOTIONAL DISORDER", raw_trr$SUBJECT_CONDITION_NEW_TRR, ignore.case = TRUE) |
+    grepl("MENTAL HEALTH", raw_trr$`Type Activity New TRR`, ignore.case = TRUE),
+]
+
+# make copy of the data with relevant demographic information
+working_data <-subset(mh, select = c(`REPORT NO`, INCIDENTDATETIME, `STREET NO`, DIR, `STREET NAME`, CITY, STATE, `ZIP CODE`, `LOCATION CODE`, BEAT, WARD, COMMUNITY, EVENT_NO,SUBJSEX, SUB_RACE, SUB_BIRTHYEAR, SUB_ZIPCODE, SUBJECT_CONDITION_NEW_TRR, SUBJECT_INJURED, SUBJECT_ALLEGED_INJURY, `Subject Med Treatment New TRR`, `Subject Actions`, SUBJECT_HOSPITALIZED, SUBJECT_ARMED, SUBJECTARMEDWITH, WEAPON_PERCEIVED_AS, SUBJECT_WEAPON_USE, `Subject Weapon`, SUBDRUGRELATED, `Type Activity New TRR`, `ReasonForResponse New TRR`, `ForceMitigationEfforts New TRR`, `ControlTactics New TRR`))
+
+# clean incident date + gen year variable
+working_data$INCIDENTDATETIME <- strptime(working_data$INCIDENTDATETIME, format = "%d-%b-%Y %H:%M")
+working_data$year <- format(working_data$INCIDENTDATETIME, "%Y")
+
+# get counts by year (sanity check)
+year_summary <- working_data %>%
+  group_by(year) %>%
+  summarise(Count = n()) %>%
+  arrange(year)
+rm(year_summary) # drop df
+
+# group by racial demographics and ward
+subject_demographics <- working_data %>%
+  group_by(COMMUNITY, SUB_RACE) %>%
+  summarise(Count = n(), .groups = "drop") %>%
+  arrange(COMMUNITY, SUB_RACE)
+
+# reshape data wide for flourish
+subj_dem_wide <- subject_demographics %>%
+  pivot_wider(
+    names_from = SUB_RACE,
+    values_from = Count,
+    values_fill = 0  
+  )
+
+# export data
+writexl::write_xlsx(subj_dem_wide, 'Processed Data/Subject Demographics_Mental Health Flag_TRRs.xlsx')
+
+# end of file
